6fa96cc91df0c02b493a768d56faabc739565190
PatriksM <pmorevs@gmail.com>
2025-01-10 10:05:34 +0200

    basic structure started

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <div id="wrapper">
+        <div id="menu">M E N U</div>
+        <div id="chatbox"> C h a t B o x</div>
+        <form name="message" action="">
+            <input name="usermsg" type="text" id="usermsg"></input>
+            <input name="submitmsg" type="submit" id="submit" value="Send"></input>
+        </form>
+    </div>
 </body>
 </html>
